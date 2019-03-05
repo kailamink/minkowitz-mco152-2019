fc7252a5ba2b788d67ee391816a2f70fd6d06f93
Kaila Minkowitz <kailamink@gmail.com>
2019-03-05 18:33:01 -0500

    started working on bananagrams

---
+++ src/main/java/minkowitz/bananagrams/Bananagrams.java
@@ -0,0 +1,36 @@
+package minkowitz.bananagrams;
+
+import minkowitz.dictionary.Dictionary;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bananagrams {
+
+    public ArrayList<String> myLetters = new ArrayList<>();
+    public final int NR_OF_LETTERS = 14;
+    Dictionary dictionary;
+
+    public Bananagrams ()
+    {
+        initializeMyLetters();
+    }
+
+    public ArrayList<String> getAllViableWords() throws FileNotFoundException {
+        ArrayList<String> viableWords = new ArrayList<>();
+        dictionary = new Dictionary();
+        
+    }
+    private void initializeMyLetters()
+    {
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random rand = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int ix = 0; ix < NR_OF_LETTERS; ++ix)
+        {
+            char chr = letters.charAt(rand.nextInt(letters.length()));
+            myLetters.add(Character.toString(chr));
+        }
+    }
+}
